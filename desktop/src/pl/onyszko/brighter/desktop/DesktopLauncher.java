079aa858e90978d2e190c941c0e6ee7da682ea48
Sławomir <slawomir@onyszko.pl>
2016-01-21 00:51:47 +0100

    init commit

---
+++ desktop/src/pl/onyszko/brighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.onyszko.brighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.onyszko.brighter.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
