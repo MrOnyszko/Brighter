593b06bca5da0f0fb49cfa37cc2af74d5c3d5fa7
Sławomir Onyszko <slawomir.onyszko@gmail.com>
2016-02-23 23:21:12 +0100

    Project organization. States manager.
    And so on...

--- core/src/pl/onyszko/brighter/Main.java
+++ core/src/pl/onyszko/brighter/Main.java
@@ -3,25 +3,37 @@ package pl.onyszko.brighter;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import pl.onyszko.brighter.states.MenuState;
+import pl.onyszko.brighter.utils.GameStateManager;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+
+    private GameStateManager mGSM;
 
 	@Override
+	public void create () {
+        mGSM = new GameStateManager();
+        mGSM.push(new MenuState(mGSM));
+	}
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        mGSM.resize(width, height);
+    }
+
+    @Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        mGSM.update(Gdx.graphics.getDeltaTime());
+        mGSM.render();
 	}
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        mGSM.dispose();
+    }
 }
--- desktop/src/pl/onyszko/brighter/desktop/DesktopLauncher.java
+++ desktop/src/pl/onyszko/brighter/desktop/DesktopLauncher.java
@@ -3,10 +3,14 @@ package pl.onyszko.brighter.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import pl.onyszko.brighter.Main;
+import pl.onyszko.brighter.utils.Dictionary;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		config.title = Dictionary.GAME_TITLE;
+        config.width = Dictionary.VIRTUAL_WIDTH;
+        config.height = Dictionary.VIRTUAL_HEIGHT;
+        new LwjglApplication(new Main(), config);
 	}
 }
